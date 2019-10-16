6906162ce1b96d3a9e61e5b66c555426d86861a8
Aleksandr <gosukudemus@gmail.com>
2019-10-16 20:50:49 +0500

    v4

--- file.cpp
+++ file.cpp
@@ -3,9 +3,18 @@
 using namespace std;
 
 int main(){
-int a;
-cout<<"¬ведите"<<endl;
-cin>>a;
-cout<<"Hello, "<<a<<endl;
-return 0;
+   char str[100];
+   bool IsBreak=false;
+   cout<<"¬ведите"<<endl;
+   cin>>str;
+   for (int i=0; str[i]!='\0'; i++)
+   if (str[i]<48 || str[i]>57) {
+      cout<<"¬ведено не число\n"; 
+      IsBreak=true; 
+      break;
+   }
+   if(IsBreak==false){
+      cout<<"Hello, "<<str<<endl;
+   }
+   return 0;
 }
